88cdf5938650c52529c0f135faa9facada462d0b
ANKIT KUMAR <103140619+XNKIT-OS@users.noreply.github.com>
2022-04-07 12:41:55 +0530

    Update owner.py

--- userbot/plugins/owner.py
+++ userbot/plugins/owner.py
@@ -26,7 +26,7 @@ async def _(event):
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
     await event.edit(
-        """My Owner @OS_xD,`A Passionate Developer From India✌️Python,Java,Linux💙`.Sponsor By `RC`"""
+        """My Owner @AnKiT_KuMaR_xD,`A Passionate Developer From India✌️Python,Java,Linux💙`.Sponsor By `RC`"""
     )
 
 
